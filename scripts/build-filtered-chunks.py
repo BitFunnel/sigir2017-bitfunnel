c523b80c8cc169c083258416b77642d450a67053
Michael Hopcroft <mhop@microsoft.com>
2017-04-30 14:24:50 -0700

    Cleaned up build-filtered-chunks.py.

--- scripts/build-filtered-chunks.py
+++ scripts/build-filtered-chunks.py
@@ -5,74 +5,15 @@ import stat
 import subprocess
 
 
-def run(working_directory, command, params = None):
-    if params is None:
-        params = []
-    args = [command] + params
-    print("{0:<50}: {1}".format(working_directory,
-                                ' '.join(args)))
-    proc = subprocess.Popen(args,
-                            cwd = working_directory,
-                            stdout = subprocess.PIPE,
-                            stderr = subprocess.PIPE)
+def run(args, working_directory):
+    proc = subprocess.Popen(args, cwd=working_directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True);
     for line in proc.stdout:
         print(line.decode(), end='')
     proc.stdout.close()
     returncode = proc.wait()
 
-
-def run_with_pipe2(working_directory, command1, params1, command2, params2):
-    args1 = [command1] + params1
-    args2 = [command2] + params2
-    print("{0:<50}: {1} | {2}".format(working_directory,
-                                      ' '.join(args1),
-                                      ' '.join(args2)))
-
-    proc1 = subprocess.Popen(args1,
-                            cwd = working_directory,
-                            stdout = subprocess.PIPE,
-                            stderr = subprocess.PIPE, shell=True)
-
-    proc2 = subprocess.Popen(args2,
-                            cwd = working_directory,
-                            stdin = proc1.stdout,
-                            stdout = subprocess.PIPE,
-                            stderr = subprocess.PIPE)
-
-    proc1.communicate();
-    proc1.stdout.close();
-
-    for line in proc2.stdout:
-        print(line.decode(), end='')
-    proc2.stdout.close()
-    returncode = proc2.wait()
-
-
-def run_with_pipe(working_directory, command1, params1, command2, params2):
-    args1 = [command1] + params1
-    args2 = [command2] + params2
-    print("{0:<50}: {1} | {2}".format(working_directory,
-                                      ' '.join(args1),
-                                      ' '.join(args2)))
-
-    proc1 = subprocess.Popen(args1,
-                            cwd = working_directory,
-                            stdout = subprocess.PIPE,
-                            stderr = subprocess.PIPE, shell=True)
-
-    proc2 = subprocess.Popen(args2,
-                            cwd = working_directory,
-                            stdin = proc1.stdout,
-                            stdout = subprocess.PIPE,
-                            stderr = subprocess.PIPE)
-
-    proc1.communicate();
-    proc1.stdout.close();
-
-    for line in proc2.stdout:
-        print(line.decode(), end='')
-    proc2.stdout.close()
-    returncode = proc2.wait()
+    # Ensure we don't get zombie processes. This had been a problem with the 7z decompression.
+    del proc
 
 
 class Builder:
@@ -84,6 +25,8 @@ class Builder:
         self.min_postings = min_postings
         self.max_postings = max_postings
 
+        # Use a slightly unusual temp file name to protect against accidental
+        # deletions of other directories with the same name.
         self.temp = os.path.join(root, "tempxxx");
         self.chunkdir = os.path.join(root, "chunks")
         self.classpath = os.path.join(self.mg4j, "target", "mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar")
@@ -101,18 +44,11 @@ class Builder:
 
     def decompress(self, chunk):
         input = os.path.join(self.gov2, chunk + ".7z");
-#        print("  7z x " + input)
-#        run(self.temp, "7z", ['x', input])
 
         args = ("7z x {0}").format(input);
         print(args)
 
-        proc = subprocess.Popen(args, cwd=self.temp, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True);
-        for line in proc.stdout:
-            print(line.decode(), end='')
-        proc.stdout.close()
-        returncode = proc.wait()
-        del proc
+        run(args, self.temp);
 
 
     def build_collection(self, chunk):
@@ -122,7 +58,6 @@ class Builder:
         print("  Creating mg4j collection from " + input)
 
         input = os.path.join(self.temp, chunk, "*.txt")
-        #classpath = os.path.join(self.mg4j, "target", "mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar")
         output = os.path.join(self.temp, chunk + ".collection")
         args = ("dir /s/b {0} | "
                 "java -cp {1} "
@@ -133,11 +68,7 @@ class Builder:
 
         print(args)
 
-        proc = subprocess.Popen(args, cwd = self.temp, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True);
-        for line in proc.stdout:
-            print(line.decode(), end='')
-        proc.stdout.close()
-        returncode = proc.wait()
+        run(args, self.temp);
 
 
     def create_chunk(self, chunk):
@@ -150,11 +81,7 @@ class Builder:
 
         print(args)
 
-        proc = subprocess.Popen(args, cwd=self.temp, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True);
-        for line in proc.stdout:
-            print(line.decode(), end='')
-        proc.stdout.close()
-        returncode = proc.wait()
+        run(args, self.temp);
 
 
     def create_chunk_manifest(self, chunk):
@@ -165,24 +92,13 @@ class Builder:
             file.write(input + '\n')
 
 
-    # def create_shard_definition(self):
-    #     # Create the ShardDefinition file
-    #     output = os.path.join(self.chunkdir, "ShardDefinition.csv")
-    #     with open(output, 'w') as file:
-    #         file.write('\n')
-
-
     def create_filtered_chunk(self, chunk):
         manifest = os.path.join(self.temp, "manifest.txt")
         args = ("{0} filter {1} {2} -size {3} {4}").format(
             self.bitfunnel, manifest, self.chunkdir, self.min_postings, self.max_postings)
         print(args)
 
-        proc = subprocess.Popen(args, cwd=self.chunkdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True);
-        for line in proc.stdout:
-            print(line.decode(), end='')
-        proc.stdout.close()
-        returncode = proc.wait()
+        run(args, self.temp);
 
 
     def rename_filtered_chunk(self, chunk):
@@ -222,23 +138,15 @@ class Builder:
         basenames = [os.path.splitext(f)[0] for f in os.listdir(self.gov2) if os.path.isfile(os.path.join(self.gov2, f)) and f.endswith('.7z')]
 
         self.cleanup()
-#        self.process_one_chunk(basenames[0]);
-#        self.process_one_chunk(basenames[1]);
 
         for chunk in basenames:
             self.process_one_chunk(chunk)
 
 builder = Builder(r"d:\data\gov2",
-                  r"d:\sigir",
+                  r"d:\temp",
+#                  r"d:\sigir",
                   r"D:\git\mg4j-workbench",
                   r"D:\git\BitFunnel\build-msvc\tools\BitFunnel\src\Release\BitFunnel.exe",
                   100,
                   150);
 builder.go();
-
-
-
-# self.basenames = [os.path.splitext(f)[0] for f in glob(os.path.join(self.gov2, "*.7z"))]
-# self.basenames = [f
-#          for root, dirs, files in os.listdir(self.gov2)
-#          for f in files]
