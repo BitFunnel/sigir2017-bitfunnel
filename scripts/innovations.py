4b1b8812fba55fe07c00e90b44bb9528f8065a12
Michael Hopcroft <mhop@microsoft.com>
2017-05-10 03:49:34 -0700

    Innovations table latex.

--- scripts/innovations.py
+++ scripts/innovations.py
@@ -3,13 +3,6 @@ from experiment import Experiment
 import os
 import re
 
-# for each term table configuration:
-#   for each density:
-#     make a directory
-#     build new term table
-#     run qps measurement
-#     extract qps and bits/posting
-#     write out results
 
 def measure_innovations(experiment, treatments, densities):
     bf_index_path = os.path.join(experiment.bf_index_path, "innovations")
@@ -27,7 +20,7 @@ def measure_innovations(experiment, treatments, densities):
                                                    bf_index_path)
     statistics_log = os.path.join(bf_index_path, "statistics-log.txt")
     print(args)
-    # run(args, bf_index_path, statistics_log)
+    run(args, bf_index_path, statistics_log)
 
     # Make the repl script
     repl_script = os.path.join(bf_index_path, "repl-script")
@@ -59,11 +52,17 @@ def measure_innovations(experiment, treatments, densities):
 
 
 def analyze_innovations(experiment, labels, treatments, densities):
-    print("{:<10} {:>10} {:>10} {:>10} {:>10}".format("Treatment",
-                                                      "Density",
-                                                      "Bits",
-                                                      "QPS",
-                                                      "DQ"))
+    print(r"\begin{table}[h]")
+    print(r"    \caption{Impact of \bitFunnel{} Innovations.}")
+    print(r"    \vspace{ - 04 mm}")
+    print(r"    \label{tab:compare-algorithms}")
+    print(r"    \begin{tabular}{cccrr}")
+    print(r"        {:<10} & {:>10} & {:>10} & {:>10} & {:>10}\\".format("Treatment",
+                                                                         "Density",
+                                                                         "Bits/Posting",
+                                                                         "kQPS",
+                                                                         "DQ"))
+    print(r"        \hline")
 
     bf_index_path = os.path.join(experiment.bf_index_path, "innovations")
 
@@ -74,12 +73,11 @@ def analyze_innovations(experiment, labels, treatments, densities):
                 run_queries_log = myfile.read()
                 bpp = float(re.findall("Bits per posting: (\d+\.?\d+)", run_queries_log)[0])
                 qps = float(re.findall("QPS: (\d+\.?\d+)", run_queries_log)[0])
-                print("{:<10} {:>10.2f} {:>10.2f} {:>10,.0f} {:>10,.0f}".format(labels[i],
-                                                  density,
-                                                  bpp,
-                                                  qps,
-                                                  qps / bpp))
-        print()
+                print(r"        {:<10} & {:>10.2f} & {:>10.1f} & {:>10,.1f} & {:>10,.0f} \\".format(
+                    labels[i], density, bpp, qps / 1000.0, qps / bpp))
+        print(r"        \hline")
+    print(r"    \end{tabular}")
+    print(r"\end{table}")
 
 
 def innovations(experiment, labels, treatments, densities):
@@ -120,7 +118,7 @@ densities = [0.05, 0.10, 0.15, 0.20, 0.25]
 
 # labels =["BSS"]
 # treatments = ["ClassicBitsliced"]
-# densities = [0.10]
+# densities = [0.05]
 
 
 innovations(experiment_windows_273_1024_2047, labels, treatments, densities)
